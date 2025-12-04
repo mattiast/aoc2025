6b51f5f6ad9b23112c375a1d039da1de47252969
Matti Åstrand <mattiastr@gmail.com>
2025-12-03 21:03:48 -0500

    stub day 3

--- src/days/mod.rs
+++ src/days/mod.rs
@@ -2,11 +2,13 @@ use crate::Solution;
 
 pub mod day01;
 pub mod day02;
+pub mod day03;
 
 pub fn get_solution(day: u8) -> Option<Box<dyn Solution>> {
     match day {
         1 => Some(Box::new(day01::Day01)),
         2 => Some(Box::new(day02::Day02)),
+        3 => Some(Box::new(day03::Day03)),
         _ => None,
     }
 }
