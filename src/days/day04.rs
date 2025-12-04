a6b654fc6e662eede852c520019489d3fb12b03a
Matti Åstrand <mattiastr@gmail.com>
2025-12-04 07:32:39 -0500

    Stub for day 4

---
+++ src/days/day04.rs
@@ -0,0 +1,13 @@
+use crate::Solution;
+
+pub struct Day04;
+
+impl Solution for Day04 {
+    fn part1(&self, _input: &str) -> String {
+        "Part 1 TODO".to_string()
+    }
+
+    fn part2(&self, _input: &str) -> String {
+        "Part 2 TODO".to_string()
+    }
+}
--- src/days/mod.rs
+++ src/days/mod.rs
@@ -3,12 +3,14 @@ use crate::Solution;
 pub mod day01;
 pub mod day02;
 pub mod day03;
+pub mod day04;
 
 pub fn get_solution(day: u8) -> Option<Box<dyn Solution>> {
     match day {
         1 => Some(Box::new(day01::Day01)),
         2 => Some(Box::new(day02::Day02)),
         3 => Some(Box::new(day03::Day03)),
+        4 => Some(Box::new(day04::Day04)),
         _ => None,
     }
 }
