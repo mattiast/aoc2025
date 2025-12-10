88f7dafe22086249f95bda0e6c4457325f4e3883
Matti Åstrand <mattiastr@gmail.com>
2025-12-10 00:35:41 -0500

    Day 10 part 1 works

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -1,12 +1,16 @@
 use crate::Solution;
 
-pub struct Day10;
-
-#[derive(Debug, Clone)]
-struct Device {
-    pattern: Vec<bool>, // true = on (#), false = off (.)
-    groups: Vec<Vec<usize>>,
-    joltages: Vec<u32>,
+fn group_to_bitmask(group: &Vec<usize>) -> u16 {
+    group.iter().fold(0, |acc, &n| acc | (1 << n))
+}
+fn pattern_to_bitmask(pattern: &Vec<bool>) -> u16 {
+    pattern.iter().enumerate().fold(0, |acc, (i, &b)| {
+        if b {
+            acc | (1 << i)
+        } else {
+            acc
+        }
+    })
 }
 
 fn parse_pattern(s: &str) -> Vec<bool> {
@@ -74,10 +78,43 @@ fn parse_input(input: &str) -> Vec<Device> {
     input.lines().filter_map(|line| parse_line(line)).collect()
 }
 
+#[derive(Debug, Clone)]
+struct Device {
+    pattern: Vec<bool>, // true = on (#), false = off (.)
+    groups: Vec<Vec<usize>>,
+    joltages: Vec<u32>,
+}
+
+/// Given a pattern and a group of patterns, find the minimum
+/// size of a subset of the group, whose XOR equals the pattern.
+fn min_repr_pattern(pattern: u16, group: &Vec<u16>) -> u16 {
+    let mut dp = vec![u16::MAX; 1 << 10];
+    dp[0] = 0;
+
+    for &g in group {
+        for p in 0..dp.len() {
+            let new_p = p ^ g as usize;
+            if dp[p] != u16::MAX {
+                dp[new_p] = dp[new_p].min(dp[p] + 1);
+            }
+        }
+    }
+
+    dp[pattern as usize]
+}
+
+pub struct Day10;
+
 impl Solution for Day10 {
     fn part1(&self, input: &str) -> String {
         let devices = parse_input(input);
-        format!("Parsed {} devices", devices.len())
+        let mut total = 0;
+        for device in &devices {
+            let g = device.groups.iter().map(|g| group_to_bitmask(g)).collect();
+            let min_size = min_repr_pattern(pattern_to_bitmask(&device.pattern), &g);
+            total += min_size as u32;
+        }
+        format!("Parsed {} devices, total min size: {}", devices.len(), total)
     }
 
     fn part2(&self, _input: &str) -> String {
